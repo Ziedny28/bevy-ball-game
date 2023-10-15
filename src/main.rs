933e40dc7a1615225cabe3447a7527db62a0df45
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-10-15 10:32:54 +0700

    enemy random movement and sound effect

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
 
+// player's variable
 pub const PLAYER_SPEED: f32 = 500.0;
 pub const PLAYER_SIZE: f32 = 64.0; //player sprite size
+
+//enemy's variable
 pub const NUMBER_OF_ENEMIES: usize = 4;
+pub const ENEMY_SPEED: f32 = 200.0;
+pub const ENEMY_SIZE: f32 = 64.0; //enemy sprite size
 
 fn main() {
     App::new()
@@ -13,7 +18,10 @@ fn main() {
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_enemies)
         .add_system(player_movement)
+        .add_system(enemy_movement)
+        .add_system(update_enemy_direction)
         .add_system(confine_player_movement)
+        .add_system(cofine_enemy_movement)
         .run();
 }
 
@@ -23,12 +31,16 @@ pub struct Player {}
 
 //entity enemy
 #[derive(Component)]
-pub struct Enemy {}
+pub struct Enemy {
+    pub direction: Vec2, //keeping track of enemy's direction
+}
 
-//system untuk spawn player dengan parameter
-// - commands untuk memberikan command,
-// - window_query untuk mendapatkan width dan height pada window,
-// - resources asset_server untuk menggunakan asset
+/*
+system untuk spawn player dengan parameter
+ - commands untuk memberikan command,
+ - window_query untuk mendapatkan width dan height pada window,
+ - resources asset_server untuk menggunakan asset
+ */
 pub fn spawn_player(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -48,10 +60,12 @@ pub fn spawn_player(
     ));
 }
 
-// system untuk spawn enemy dengan parameter
-// - commands untuk memberikan command,
-// - window_query untuk mendapatkan width dan height pada window,
-// - resources asset_server untuk menggunakan asset
+/*
+ system untuk spawn enemy dengan parameter
+ - commands untuk memberikan command,
+ - window_query untuk mendapatkan width dan height pada window,
+ - resources asset_server untuk menggunakan asset
+*/
 pub fn spawn_enemies(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -72,14 +86,18 @@ pub fn spawn_enemies(
                 texture: asset_server.load("sprites/ball_red_large.png"),
                 ..default()
             },
-            Enemy {},
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(), //give diraction random value and then normalize
+            },
         ));
     }
 }
 
-//system untuk spawn camera dengan parameter
-// - commands untuk memberikan command,
-// - window_query untuk mendapatkan width dan height pada window
+/*
+system untuk spawn camera dengan parameter
+ - commands untuk memberikan command,
+ - window_query untuk mendapatkan width dan height pada window
+*/
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap(); //mendapatkan referensi pada window
 
@@ -89,10 +107,12 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });
 }
 
-//system untuk spawn player dengan parameter
-//- keyboard input dengan tipe Resource Input Keycode untuk mengambil input,
-// - player_query, untuk mendapat player dengan cara mendapatkan transform yang memiliki player,
-// - dan time untuk normalisasi waktu nanti
+/*
+system untuk spawn player dengan parameter
+- keyboard input dengan tipe Resource Input Keycode untuk mengambil input,
+ - player_query, untuk mendapat player dengan cara mendapatkan transform yang memiliki player(transform mut karena kita akan modifikasi value variable yang ditunjuk),
+ - dan time untuk normalisasi waktu nanti
+*/
 pub fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
     mut player_query: Query<&mut Transform, With<Player>>,
@@ -126,9 +146,11 @@ pub fn player_movement(
     }
 }
 
-//system untuk membuat player tetap ada dalam layar dengan parameter
-// - player_query, untuk mendapat player dengan cara mendapatkan transform yang memiliki player,
-// - window_query untuk mendapatkan width dan height pada window,
+/*
+ system untuk membuat player tetap ada dalam layar dengan parameter
+ - player_query, untuk mendapat player dengan cara mendapatkan transform yang memiliki player(transform mut karena kita akan modifikasi value variable yang ditunjuk),
+ - window_query untuk mendapatkan width dan height pada window,
+*/
 pub fn confine_player_movement(
     mut player_query: Query<&mut Transform, With<Player>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -161,5 +183,115 @@ pub fn confine_player_movement(
         }
 
         player_transform.translation = translation;
+    }
+}
+
+/*
+ system untuk menggerakkan enemy, dengan parameter
+ - enemy_query, untuk mendapat enemy dengan cara mendapatkan transform yang memiliki enemy(transform mut karena kita akan modifikasi value variable yang ditunjuk),
+ - time, untuk gerakan independen
+*/
+pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Res<Time>) {
+    //loop on every enemy and get transform and enemy component
+    for (mut transform, enemy) in enemy_query.iter_mut() {
+        let direction = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0); //create vector 3 with random value for enemy's direction
+        transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
+        //move the enemy
+    }
+}
+
+/* system untuk membuat enemy tetap ada dalam layar dengan parameter
+ - enemy_query, untuk mendapat enemy dengan cara mendapatkan transform yang memiliki enemy,(enemy mut karena kita akan modifikasi value variable yang ditunjuk)
+ - window_query untuk mendapatkan width dan height pada window,
+ - audio, untuk efek suara
+ - asset server untuk sprite
+*/
+
+pub fn update_enemy_direction(
+    mut enemy_query: Query<(&Transform, &mut Enemy)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    audio: Res<Audio>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap(); //mendapatkan referensi pada window
+
+    let half_enemy_size = ENEMY_SIZE / 2.0; //mendapatkan ukuran setengah dari enemy
+
+    //batas posisi
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+
+    //loop untuk tiap enemy
+    for (transform, mut enemy) in enemy_query.iter_mut() {
+        let mut direction_changed: bool = false; //untuk mendeteksi apakah arah berubah
+
+        let translation = transform.translation; //current position
+
+        //jika posisi diluar batas, kalikan direction dengan -1 untuk memberi efek memantul
+        if translation.x < x_min || translation.x > x_max {
+            enemy.direction.x *= -1.0;
+            direction_changed = true;
+        }
+        if translation.y < y_min || translation.y > y_max {
+            enemy.direction.y *= -1.0;
+            direction_changed = true;
+        }
+
+        if direction_changed {
+            //mengambil beberapa audio asset
+            let sound_effect_1 = asset_server.load("audio/pluck_001.ogg");
+            let sound_effect_2 = asset_server.load("audio/pluck_002.ogg");
+
+            //memilih sound effect secara random
+            let sound_effect = if random::<f32>() > 0.5 {
+                sound_effect_1
+            } else {
+                sound_effect_2
+            };
+
+            //memainkan audio
+            audio.play(sound_effect);
+        }
+    }
+}
+
+/*
+ system for fixing bug of enemy stuck in the corner, with param
+ -enemy_query is  untuk mendapat enemy dengan cara mendapatkan transform yang memiliki enemy,(transform mut karena kita akan modifikasi value variable yang ditunjuk)
+ - window_query to get screen size
+*/
+pub fn cofine_enemy_movement(
+    mut enemy_query: Query<&mut Transform, With<Enemy>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap(); //mendapatkan referensi pada window
+
+    let half_enemy_size: f32 = ENEMY_SIZE / 2.0;
+
+    let x_min: f32 = 0.0 + half_enemy_size;
+    let x_max: f32 = window.width() - half_enemy_size;
+    let y_min: f32 = 0.0 + half_enemy_size;
+    let y_max: f32 = window.height() - half_enemy_size;
+
+    for mut transform in enemy_query.iter_mut() {
+        let mut translation = transform.translation; //current position
+
+        //bound enemy's x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        //bound enemy's y position
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        transform.translation = translation;
     }
 }
