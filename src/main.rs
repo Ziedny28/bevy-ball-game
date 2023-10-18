4a841320ce526b73b44bbafc183b2acb929bcba5
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-10-18 21:16:45 +0700

    spawn enemy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::app::AppExit; //for quiting app
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
@@ -10,6 +11,7 @@ pub const PLAYER_SIZE: f32 = 64.0; //player sprite size
 pub const NUMBER_OF_ENEMIES: usize = 4;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0; //enemy sprite size
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
 
 //star variable
 pub const NUMBER_OF_STARS: usize = 10;
@@ -20,7 +22,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>() // add score resource with default value and keep tracking score not delete prev value
-        .init_resource::<StarSpawnTimer>() // add score resource with default value and keep tracking
+        .init_resource::<StarSpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
+        .add_event::<GameOver>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_enemies)
@@ -35,6 +39,9 @@ fn main() {
         .add_system(update_score)
         .add_system(tick_star_spawn_timer)
         .add_system(spawn_star_overtime)
+        .add_system(tick_enemy_spawn_timer)
+        .add_system(spawn_enemies_overtime)
+        .add_system(exit_game)
         .run();
 }
 
@@ -70,13 +77,31 @@ impl Default for Score {
 pub struct StarSpawnTimer {
     pub timer: Timer,
 }
-
+//give default value
 impl Default for StarSpawnTimer {
     fn default() -> StarSpawnTimer {
         StarSpawnTimer {
             timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating), // set default value as repeat timer each one second
         }
     }
+}
+
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+//give default value
+impl Default for EnemySpawnTimer {
+    fn default() -> EnemySpawnTimer {
+        EnemySpawnTimer {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating), // set default value as repeat timer each enemy_spawn_time
+        }
+    }
+}
+
+// struct yang akan digunakan untuk event
+pub struct GameOver {
+    pub score: u32,
 }
 
 /*
@@ -380,10 +405,12 @@ pub fn cofine_enemy_movement(
 */
 pub fn enemy_hit_player(
     mut commands: Commands,
+    // mut game_over_event_writer: EventWriter<GameOver>,
     mut player_query: Query<(Entity, &Transform), With<Player>>,
     enemy_query: Query<&Transform, With<Enemy>>,
     asset_server: Res<AssetServer>,
     audio: Res<Audio>,
+    // score: Res<Score>,
 ) {
     // jika terdaat player entity dan transform
     if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
@@ -492,5 +519,46 @@ pub fn spawn_star_overtime(
             },
             Star {},
         ));
+    }
+}
+
+pub fn tick_enemy_spawn_timer(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemies_overtime(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+) {
+    //jika waktu habis
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap(); //mendapatkan referensi pada window
+
+        //get random position
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        //spawn bitang
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
+        ));
+    }
+}
+
+pub fn exit_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_event_writer.send(AppExit);
     }
 }
